df81591d6474e3a44f662e4fca000ccc81f42786
João Lauria Pires Santos <104474680+JoaoLauria@users.noreply.github.com>
2025-05-06 11:18:35 -0300

    Create cardapio.js

---
+++ screens/cardapio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+export default function Cardapio({ mesa, onVoltar }) {
+    return (
+    <div style={styles.container}>
+    <Button variant="outlined" color="primary" onClick={onVoltar} style={{ marginTop: '16px' }}>Voltar</Button>
+        <h2>Cardápio mesa: {mesa}</h2>  
+      </div>
+    );
+  }
+  
+  const styles = {
+    container: {
+      padding: '20px',
+    }
+  };
+  
