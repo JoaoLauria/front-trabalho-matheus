3a8547f4c6728aaaea388978b40fe914c3481e84
João Lauria Pires Santos <104474680+JoaoLauria@users.noreply.github.com>
2025-05-06 11:18:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import Login from './screens/login';
 import Comandas from './screens/comandas';
+import Cardapio from './screens/cardapio';
 
 export default function App() {
-  const [logado, setLogado] = useState(false);
+  const [logado, setLogado] = useState(true);
+  const [mesaSelecionada, setMesaSelecionada] = useState(null);
 
-  //return logado ? <Comandas /> : <Login onLogin={() => setLogado(true)} />;
-  return <Comandas />
+//return logado ? <Comandas /> : <Login onLogin={() => setLogado(true)} />;
+
+  if (mesaSelecionada) {
+    return <Cardapio mesa={mesaSelecionada} onVoltar={() => setMesaSelecionada(null)} />;
+  }
+
+  return <Comandas selectMesa={setMesaSelecionada} />;
 }
