4d8a0ba50d119858ea9ad2746adef2bed5cf2726
igorcenzi <igor.cenzi2@gmail.com>
2025-06-04 04:11:10 -0300

    removido comentarios

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ErrorProvider } from './contexts/ErrorContext';
 
-// Importando as telas
 import Login from './screens/login';
 import Comandas from './screens/comandas';
 import Cardapio from './screens/cardapio';
@@ -13,17 +12,14 @@ import CadastroUsuario from './screens/cadastro';
 import CadastroMesa from './screens/CadastroMesa';
 import GerenciarCategorias from './screens/GerenciarCategorias';
 
-// Criando os navegadores
 const Stack = createNativeStackNavigator();
 
-// Criando o contexto de autenticação para compartilhar o estado de login
 export const AuthContext = React.createContext();
 
 export default function App() {
   const [logado, setLogado] = useState(false);
   const [token, setToken] = useState(null);
 
-  // Verificar se o usuário já está logado ao iniciar o app
   useEffect(() => {
     const checkToken = async () => {
       try {
@@ -40,21 +36,18 @@ export default function App() {
     checkToken();
   }, []);
 
-  // Função para fazer login
   const login = (userToken) => {
     setToken(userToken);
     setLogado(true);
     localStorage.setItem('accessToken', userToken);
   };
 
-  // Função para fazer logout
   const logout = () => {
     setToken(null);
     setLogado(false);
     localStorage.removeItem('accessToken');
   };
 
-  // Contexto de autenticação
   const authContext = React.useMemo(() => ({
     login,
     logout,
@@ -68,13 +61,11 @@ export default function App() {
         <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           {!logado ? (
-            // Rotas para usuários não autenticados
             <>
               <Stack.Screen name="Login" component={Login} />
               <Stack.Screen name="Cadastro" component={CadastroUsuario} />
             </>
           ) : (
-            // Rotas para usuários autenticados
             <>
               <Stack.Screen name="Comandas" component={Comandas} />
               <Stack.Screen name="PedidosMesa" component={PedidosMesa} />
